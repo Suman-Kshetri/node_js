638df922f274c322fcf6847955f3c6da63ef5227
Suman-Kshetri <ksuman20601028@gmail.com>
2024-07-07 09:12:30 +0545

    hello this is node.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const express=require("express")//include express package in our file
+const app=express();//call the express function//app ma  object aayera baseko hunxag
+
+app.set("vew engine","ejs")
+app.get("/",(req,res)=>{
+        res.send ("<h1>haha this is home page</h1>")
+})
+// app.get("/about",(req,res)=>{
+//     res.send(`<h1>hello this is about page</h1>`)
+// })
+app.get("/about",(req,res)=>{
+    const name="suman kshetri";
+    res.render("about.ejs",{name});
+
+})
+app.get("/contact",(req,res)=>
+{
+    const name=""
+    res.render("about.ejs",{name});
+})
+
+
+app.listen(3000,()=>{//3000 vhaneko kun port number chalaune vhaneko[in terminal]
+    console.log(`Nodejs project has started ${3000}`)
+})
