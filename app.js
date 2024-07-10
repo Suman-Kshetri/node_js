91fda36d2326b46fb4a1f36a81e136fd282571c0
Suman-Kshetri <ksuman20601028@gmail.com>
2024-07-10 14:44:46 +0545

    lerned all about the backend using nodeJs

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const connectToDb = require("./database/databaseConnection"); //to connect to th
 const Blog = require("./model/blogModel");
 const app = express(); //call the express function//app ma  object aayera baseko hunxag
 
+// const multer = require("./middleware/multerConfig").multer;
+// const storage = require("./middleware/multerConfig").storage;
+const{multer,storage} = require('./middleware/multerConfig.js')
+const upload = multer({storage: storage})
+
 connectToDb(); //call funtion
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("vew engine", "ejs");
-app.get("/", (req, res) => {
-  res.send("<h1>haha this is home page</h1>");
-});
+
+app.get("/homepage",async (req,res)=>{
+  const blogs = await Blog.find()//always return array
+  console.log(blogs);
+  res.render("home.ejs",{blogs})
+})
+
+
 // app.get("/about",(req,res)=>{
 //     res.send(`<h1>hello this is about page</h1>`)
 // })
@@ -28,21 +38,53 @@ app.get("/contact", (req, res) => {
 app.get("/createblog", (req, res) => {
   res.render("create_blog.ejs");
 });
+app.get("/homepage",async (req,res)=>
+{
+  const blogs=await Blog.find();
+  console.log(blogs);
 
-app.post("/createblog", async (req, res) => {
+  res.render("home.ejs",{blogs});
+})
+app.get("/blogpage",(req,res)=>{
+  res.render("blog.ejs");
+})
+
+app.post("/createblog", upload.single('image'),async (req, res) => {
+const file=req.file.filename;
+// console.log(file)
+
   const { title, subtitle, description } = req.body;
   console.log(title, subtitle, description);
-//   console.log(req.body);
+  console.log(req.body);
   await Blog.create({
     title,
     subtitle,
     description,
+    image: file
   })
 
   //   res.render(req.body);
   res.send("Block created sucessfully");
 });
+
+app.get("/blogpage/:id",async(req,res)=>
+{
+  const id=req.params.id;
+  const blog = await Blog.findById(id);
+  console.log(blog)
+  res.render("blog.ejs",{blog})
+})
+
+app.use(express.static("./storage"));
+
 app.listen(3000, () => {
   //3000 vhaneko kun port number chalaune vhaneko[in terminal]
   console.log(`Nodejs project has started ${3000}`);
 });
+
+app.get("/deleteblog/:id",async (req,res)=>
+{
+  const id=req.params.id;
+  await Blog.findByIdAndDelete(id)
+  res.redirect("/homepage")
+})
